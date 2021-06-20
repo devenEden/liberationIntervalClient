0fa9f46c786bcb7a3f53138038fa43b2d9a6f40e
Feta Deven <deshdeven@gmail.com>
2021-06-20 23:03:06 +0300

    added server url

--- src/reducers/globalReducer.js
+++ src/reducers/globalReducer.js
@@ -1,6 +1,6 @@
 import { globalConstants } from "../actions/global/globalActions";
 const initialState = {
-  api_url: "http://127.0.0.1:5000",
+  api_url: "https://liberation-interval.herokuapp.com/",
 
   authLayout: "login",
   isForgotPasswordModalOpen: false,
