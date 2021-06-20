787c49115ef4dc6f0c081735302e21ef01ef7c6c
Feta Deven <deshdeven@gmail.com>
2021-06-20 23:37:59 +0300

    removed useState from card container

--- src/components/mixtapes/CardContainer.js
+++ src/components/mixtapes/CardContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Button, Card, Empty, message } from "antd";
 import img from "../../assets/s263939.jpg";
 import { useDispatch, useSelector } from "react-redux";
