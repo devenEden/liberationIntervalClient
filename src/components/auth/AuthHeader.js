beb6849563778efdf1fdc6ceacda00ac5150f577
Feta Deven <deshdeven@gmail.com>
2021-06-20 23:28:01 +0300

    solved history bug in auth header component

--- src/components/auth/AuthHeader.js
+++ src/components/auth/AuthHeader.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Tooltip } from "antd";
 import { IoMdHome } from "react-icons/io";
-import { useHistory } from "react-router-dom";
 
 const AuthHeader = () => {
-  const history = useHistory();
 
   const goToLandingPage = () => {
    window.location = "/";
